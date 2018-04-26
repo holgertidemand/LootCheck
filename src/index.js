da4e8fd588b0af3b993e328952f58a93a15641cd
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-26 11:06:33 +0200

    adds wallet component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux'; 
+import balanceReducer from './reducers/reducer_balance';
+import { Provider } from 'react-redux'; 
 import App from './components/app';
 
-render(<App />, document.getElementById('root'));
+const store = createStore(balanceReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
